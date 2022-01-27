a98965cb118286d9dc777b58bf377544a69a9206
Kiryl Laptanovich <laptanovi4@gmail.com>
2022-01-27 09:29:48 +0100

    fixes in config creation

--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -27,6 +27,7 @@ then
 
     docker exec -ti cyberindex_postgres psql -f /root/schema/05-grid.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
     docker exec -ti cyberindex_postgres psql -f /root/schema/06-resources.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
+    docker exec -ti cyberindex_postgres psql -f /root/schema/07-wasm.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
 
     # init additional views and table with old precommits
     docker exec -ti cyberindex_postgres psql -f /root/schema/views.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
