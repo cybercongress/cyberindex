0292302c3dbe79b49cf5320727761183a0c956d3
Ales Puchilo <ales.puchilo@gmail.com>
2022-06-29 13:11:48 +0200

    Finalized clean up

--- README.md
+++ README.md
@@ -36,3 +36,65 @@ Thanks for supporting Cosmos's ecosystem development:
 - Desmos Labs [Juno](https://github.com/desmos-labs/juno) 
 - Forbole [BDJuno](https://github.com/forbole/bdjuno) 
 - Juno Community [Wasmx](https://github.com/disperze/wasmx)
+
+## Analytics views
+
+If you want to add analytics views to you index:
+
+Download genesis account states:
+
+```bash
+wget -O database/schema/genesis.csv https://gateway.ipfs.cybernode.ai/ipfs/QmWxvLnFZDJUrjTjNDt4BfanzncdbzTMfSQmkNAACQ8ZaF
+```
+
+Inititate the additional views and tables:
+
+```bash
+docker exec -ti cyberindex_postgres psql -f /root/schema/views.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
+```
+
+Copy genesis and cyber_gift from csv to table:
+
+```bash
+docker exec -ti cyberindex_postgres psql -c "\copy genesis FROM /root/schema/genesis.csv with csv HEADER" -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
+```
+
+Add cronjob to refresh tables for stats:
+
+```bash
+    croncmd="docker exec -t cyberindex_postgres psql -c \"REFRESH MATERIALIZED VIEW CONCURRENTLY txs_ranked\" -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME"
+    cronjob="*/5 * * * * $croncmd"
+    ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
+```
+
+## Cybergift table with proofs
+
+If you want to add cybergift table with proofs:
+
+Download cybergift with proofs file:
+
+```bash
+wget -O database/schema/cyber_gift_proofs.csv https://gateway.ipfs.cybernode.ai/ipfs/HASH
+```
+
+Inititate the `cyber_gift_proofs` table:
+
+```bash
+docker exec -ti cyberindex_postgres psql -f /root/schema/cyber_gift.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
+```
+
+Go to `./database/schema/` folder.
+
+Open `gift_db.py` file and fill global variables according to your set up
+
+Install `requirenments.txt`
+
+```bash
+pip3 install -r requirenments.txt
+```
+
+Run script:
+
+```bash
+python3 gift_db.py
+````
--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -8,13 +8,6 @@ then
     # temporeraly import variables
     export $(cat .env)
 
-    # get static gift and genesis tables
-
-    if [ ! -f "database/schema/genesis.csv" ]
-    then
-        wget -O database/schema/genesis.csv https://gateway.ipfs.cybernode.ai/ipfs/QmWxvLnFZDJUrjTjNDt4BfanzncdbzTMfSQmkNAACQ8ZaF
-    fi
-
     # build cyberindexer and run it in container
     docker build -t cyberindex:latest .
 
@@ -35,28 +28,7 @@ then
     docker exec -ti cyberindex_postgres psql -f /root/schema/06-resources.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
     docker exec -ti cyberindex_postgres psql -f /root/schema/07-wasm.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
 
-    # init additional views and tables
-    docker exec -ti cyberindex_postgres psql -f /root/schema/views.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
-    docker exec -ti cyberindex_postgres psql -f /root/schema/delegation_strategy.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
-
-    # copy genesis and cyber_gift from csv to table
-    docker exec -ti cyberindex_postgres psql -c "\copy genesis FROM /root/schema/genesis.csv with csv HEADER" -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
-
-    # init gift with proofs table and copy it to postgress
-    docker exec -ti cyberindex_postgres psql -f /root/schema/cyber_gift.sql -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
-    docker exec -ti cyberindex_postgres psql -c "\copy cyber_gift_proofs FROM /root/schema/cyber_gift_proofs.csv WITH (DELIMITER ',', HEADER, FORMAT CSV, QUOTE '\"')" -d $POSTGRES_DB_NAME -U $POSTGRES_USER_NAME
-
     docker run -d --name cyberindex --network="host" -v $HOME/.cyberindex:/root/.cyberindex cyberindex:latest
-
-    # add cronjob to refresh tables for stats
-
-    croncmd="docker exec -t cyberindex_postgres psql -c \"REFRESH MATERIALIZED VIEW CONCURRENTLY txs_ranked\" -d cyberindex -U cyber"
-    cronjob="*/5 * * * * $croncmd"
-    ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
-
-    croncmd="docker exec -t cyberindex_postgres psql -c \"REFRESH MATERIALIZED VIEW CONCURRENTLY honest_pre_commits\" -d cyberindex -U cyber"
-    cronjob="*/30 * * * * $croncmd"
-    ( crontab -l | grep -v -F "$croncmd" ; echo "$cronjob" ) | crontab -
 
 else
     echo "Done."
