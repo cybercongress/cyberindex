7ed11b7defb2b467275ce8683c66442a82b504c8
C H <litvintech@gmail.com>
2024-04-08 14:34:42 +0800

    Added indexes for cyberlinks and particles

--- database/schema/04-graph.sql
+++ database/schema/04-graph.sql
@@ -8,6 +8,11 @@ CREATE TABLE cyberlinks
     height              BIGINT                      NOT NULL REFERENCES block (height),
     transaction_hash    TEXT                        NOT NULL
 );
+CREATE INDEX cyberlinks_particle_from_index ON cyberlinks(particle_from);
+CREATE INDEX cyberlinks_particle_to_index ON cyberlinks(particle_to);
+CREATE INDEX cyberlinks_neuron_index ON cyberlinks(neuron);
+CREATE INDEX cyberlinks_height_index ON cyberlinks(height);
+CREATE INDEX cyberlinks_from_to_index ON cyberlinks(particle_from, particle_to);
 
 CREATE TABLE particles
 (
@@ -17,4 +22,7 @@ CREATE TABLE particles
     timestamp           TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     height              BIGINT                      NOT NULL REFERENCES block (height),
     transaction_hash    TEXT                        NOT NULL
-)
+);
+CREATE INDEX particles_neuron_index ON particles(neuron);
+CREATE INDEX particles_height_index ON particles(height);
+CREATE INDEX particles_neuron_particle_index ON particles(neuron, particle);
